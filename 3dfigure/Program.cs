987221d28aa27c3ec3aa5394a08c5f0b4c065b09
bilalovrevan <ravanab@code.edu.az>
2021-12-10 19:32:41 +0400

    update

--- 3dfigure/Program.cs
+++ 3dfigure/Program.cs
@@ -9,22 +9,40 @@ namespace _3dFigures
 
         public static void Main(string[] args)
         {
-                            /*
-                             * Konus
-                 S(ot) = pi x r^2
-                 V = S(ot) x H / 3
+            /*
+             * Konus
+ S(ot) = pi x r^2
+ V = S(ot) x H / 3
 
 
-                 Silindr 
-                 V = pi x r^2 x l
-                 S(ot) = pi x r^2;
-                 S(tam) = 2 x S(ot)  + 2 x pi x r x l
+ Silindr 
+ V = pi x r^2 x l
+ S(ot) = pi x r^2;
+ S(tam) = 2 x S(ot)  + 2 x pi x r x l
 
 
-                 Duzbucaqlı Paralelepiped   
-                 V = a x b x c;
-                 S(tam) = 2 x (ab + bc + ac)
-             * */
+ Duzbucaqlı Paralelepiped   
+ V = a x b x c;
+ S(tam) = 2 x (ab + bc + ac)
+* */
+            Cone cone = new Cone();
+        l1:
+            try
+            {
+                Console.Write("Konusun radiusunu daxil edin: ");
+                cone.Radius = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Konusun hundurluyunu daxil edin: ");
+                cone.Height = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"Konusun oturaciqin sahesi: {cone.OturacaqSahesi()} ");
+                Console.WriteLine($"Konusun Hecmi: {cone.ConeHecm()} ");
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("ededleri duzgun daxil edin: ");
+                goto l1;
+            }
+
             Cylinder cylinder = new Cylinder();
             l1:
             try
@@ -34,7 +52,7 @@ namespace _3dFigures
                 Console.Write("Slindrin hundurluyunu daxil edin: ");
                 cylinder.Height = Convert.ToDouble(Console.ReadLine());
                 Console.WriteLine($"Slindrin oturaciqin sahesi: {cylinder.OturacaqSahesi()} ");
-                Console.WriteLine($"Slindrin oturaciqin sahesi: {cylinder.CylinderHecm()} ");
+                Console.WriteLine($"Slindrin Hecmi: {cylinder.CylinderHecm()} ");
             }
             catch (Exception)
             {
@@ -52,8 +70,8 @@ namespace _3dFigures
                 cuboid.Width = Convert.ToDouble(Console.ReadLine());
                 Console.Write("Paralelpipedin uzunluqu daxil edin: ");
                 cuboid.Length = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine($"Paralelpipedin oturaciqin sahesi: {cuboid.OturacaqSahesi()} ");
-                Console.WriteLine($"Paralelpipedin oturaciqin sahesi: {cuboid.CuboidHecm()} ");
+                Console.WriteLine($"Paralelpipedin tam sahesi: {cuboid.CuboidTamSahesi()} ");
+                Console.WriteLine($"Paralelpipedin hecmi: {cuboid.CuboidHecm()} ");
             }
             catch (Exception)
             {
